afbc7a9a99e8c9a70efd1ba1de6e5b4051a860f1
reisvitt <reis_vitor@outlook.com>
2020-03-22 22:33:11 -0300

    feature to encrypt the password to save on database

--- src/services/server/controllers/user-ctrl.js
+++ src/services/server/controllers/user-ctrl.js
@@ -1,141 +1,128 @@
-const User = require ('../models/User')
+const User = require("../models/User");
 
-const createUser = (req, res) => {
-  const body = req.body
+const createUser = async (req, res) => {
+  const body = req.body;
 
-  if(!body){
+  //verificar se usuario eh admin ou nao
+
+  if (!body) {
     return res.status(400).json({
       success: false,
-      error: 'Você precisa enviar um usuário'
-    })
+      error: "Você precisa enviar um usuário"
+    });
   }
 
+  const user = User({ ...body });
 
-  const user = new User({...body, created_at: new Date()})
-
-  if(!user){
+  if (!user) {
     return res.status(400).json({
       success: false,
       error: "Erro ao criar usuário - Dados incorretos"
-    })
+    });
   }
 
-  user.save()
-    .then(() =>{
+  await user
+    .save()
+    .then(() => {
       return res.status(201).json({
         success: true,
         id: user._id,
+        user: user,
         message: "User created"
-      })
+      });
     })
     .catch(err => {
       return res.status(400).json({
         err,
+        success: false,
         message: "User not created!"
-      })
-    })
-}
+      });
+    });
+};
 
 const updateUser = (req, res) => {
-
-  if(!req.body){
+  if (!req.body) {
     return res.status(400).json({
       success: false,
-      error: 'Você precisa enviar um usuário'
-    })
+      error: "Você precisa enviar um usuário"
+    });
   }
 
-  User.findById(
-    req.params.id,
-    (user) => {
+  User.findById(req.params.id, user => {
+    if (user) {
+      user.set(req.body);
+      user.update_at = new Date();
 
-      if(user){
-        user.set(req.body)
-        user.update_at = new Date()
-
-        user.save()
+      user
+        .save()
         .then(() => {
           return res.status(200).json({
             success: true,
             id: user._id,
             message: "User updated"
-          })
+          });
         })
         .catch(err => {
           return res.status(404).json({
             error: err,
             message: "User not updated"
-          })
-        })
-      }
-
-      return res.status(400).json({
-        message: "User not found"
-      })
-
+          });
+        });
     }
-  )
 
-} 
+    return res.status(400).json({
+      message: "User not found"
+    });
+  });
+};
 
 const deleteUser = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
-  User.findById(
-    id,
-    (user) => {
-      if(user){
-        user.remove()
-          .then((newUser) => {
-            return res.status(200).json({
-              success: true,
-              user: newUser,
-              message: "User deleted"
-            })
-          })
-          .catch(err => {
-            return res.status(404).json({
-              error: err,
-              message: "User not deleted"
-            })
-          })
-      }
-
-      return res.status(400).json({
-        message: "User not found"
-      })
-      
-    }
-  )
-}
-
-
-const getUser = (req, res) =>{
-  const id = req.query.id
-
-  User.findById(
-    id,
-    (err, user) => {
-      if(user){
-
-        return res.status(200).json({
-          user
+  User.findById(id, user => {
+    if (user) {
+      user
+        .remove()
+        .then(newUser => {
+          return res.status(200).json({
+            success: true,
+            user: newUser,
+            message: "User deleted"
+          });
         })
-      }
-
-      return res.status(400).json({
-        message: "User not found"
-      })
+        .catch(err => {
+          return res.status(404).json({
+            error: err,
+            message: "User not deleted"
+          });
+        });
     }
-  )
 
+    return res.status(400).json({
+      message: "User not found"
+    });
+  });
+};
 
+const getUser = (req, res) => {
+  const id = req.query.id;
 
-}
+  User.findById(id, (err, user) => {
+    if (user) {
+      return res.status(200).json({
+        user
+      });
+    }
+
+    return res.status(400).json({
+      message: "User not found"
+    });
+  });
+};
 
 module.exports = {
   createUser,
   updateUser,
   deleteUser,
   getUser
-}
+};
--- src/services/server/models/User.js
+++ src/services/server/models/User.js
@@ -1,17 +1,68 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new mongoose.Schema({
-  first_name: String,
-  last_name: String,
-  cpf: Number,
-  email: String,
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  cpf: {
+    type: Number,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
   avatar: String,
-  created_at: Date,
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
   update_at: Date,
-  events: [Object],
   admin: Boolean,
-  events_admin: [Object]
-})
+  events: Array,
+  events_admin: Array
+});
 
+UserSchema.pre("save", async function(next) {
+  var user = this;
 
-module.exports = mongoose.model("User", UserSchema)
+  // gera o salt
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+
+  if (!salt) {
+    return next({
+      success: false,
+      error: "Error to create Salt"
+    });
+  }
+
+  // hash o password usando o novo salt
+  if (user.password && user.isModified("password")) {
+    const hash = await bcrypt.hash(user.password, salt);
+    if (!hash) {
+      return next({
+        success: false,
+        error: "Error to ecrypt password"
+      });
+    }
+
+    // sobrescreve o o password com o hashed
+    user.password = hash;
+
+    next();
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);

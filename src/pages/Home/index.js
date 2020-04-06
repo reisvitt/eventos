be1db37b1983fb10b91c49fec6e22c3ad790e55a
Tharcio <tciothalles@hotmail.com>
2020-04-06 16:38:26 -0300

    Nova estilizacao de paginas e pagina principal adicionada

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,83 @@
 import React from "react";
 import "./styles.css";
 import { Link } from "react-router-dom";
+import logoImg from '../../assets/logo.png'
+//import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>Page Inicial</h1>
-
+      
       <div className="container-home">
         <div className="nav">
-          <Link className="link" to="/register">
-            Sign Up
-          </Link>
-          <Link className="link" to="/login">
-            Sign In
-          </Link>
+            <img className="imgLogo" src ={logoImg} alt = "logoSite"/>
+            <input className= "searchBar" type="text" placeholder="Pesquisar evento.."></input>
+            <Link className="linkR" to="/register">
+              Sign Up 
+            </Link>
+            <Link className="linkL" to="/login">
+              Sign In
+            </Link>
+            
         </div>
       </div>
+      
+      <ul>
+        <li>
+          <strong>Titulo do evento</strong>
+          <p>Descrição do evento</p>
+          <p>Valor: {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(120)}</p>
+          <Link className="link" to="/details">
+              Detalhes
+          </Link>
+        </li>
+
+        <li>
+          <strong>Titulo do evento</strong>
+          <p>Descrição do evento</p>
+          <p>Valor: {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(120)}</p>
+          <Link className="link" to="/details">
+              Detalhes
+          </Link>
+        </li>
+
+        <li>
+          <strong>Titulo do evento</strong>
+          <p>Descrição do evento</p>
+          <p>Valor: {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(120)}</p>
+          <Link className="link" to="/details">
+              Detalhes
+          </Link>
+        </li>
+
+        <li>
+          <strong>Titulo do evento</strong>
+          <p>Descrição do evento</p>
+          <p>Valor: {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(120)}</p>
+          <Link className="link" to="/details">
+              Detalhes
+          </Link>
+        </li>
+
+        <li>
+          <strong>Titulo do evento</strong>
+          <p>Descrição do evento</p>
+          <p>Valor: {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(120)}</p>
+          <Link className="link" to="/details">
+              Detalhes
+          </Link>
+        </li>
+
+        <li>
+          <strong>Titulo do evento</strong>
+          <p>Descrição do evento</p>
+          <p>Valor: {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(120)}</p>
+          <Link className="link" to="/details">
+              Detalhes
+          </Link>
+        </li>
+      </ul>
+
     </div>
   );
 };

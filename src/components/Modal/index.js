a53ad37a7540716f73779c9393b2f8827deda845
Konai <43552023+victorkonai@users.noreply.github.com>
2020-04-27 20:38:23 -0300

    modificacoes do modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -10,24 +10,9 @@ const Modal = ({id='modal' , onClose= () => {}, children}) => {
   return(
     //vai deixar a tela escura 
   <div id="modal" className="modal" onClick={handleOutSideClick} >
-    <div className="container">
-      
-      <h1>Para continuar faça seu Login</h1>
-      
-      <p>Usuário</p>
-        <input type="text" name="" placeholder="Insira seu nome de usuário"></input>
-        <p>Senha</p>
-        <input type="password" name="" placeholder="Insira sua senha"></input>
-        <a href="#">Esqueceu sua senha?</a>
-        <a href="#">Não é cadastrado?</a>
 
+    <h1></h1>
 
-      <div class="modal-buttons">
-        <button type="button" class="btn btn-primary">Login</button>
-        <h1></h1>
-				<button type="button" class="btn btn-default" onClick={onClose} data-dismiss="modal">Fechar</button>
-			</div>
-    </div>
   </div>
   );
 };

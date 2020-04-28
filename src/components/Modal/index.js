ea86ce1ce4cdacfba57e720bdc8d5d8193116bbe
reisvitt <reis_vitor@outlook.com>
2020-04-27 21:15:08 -0300

    conexao modal com pagina de event

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.css";
 
-const Modal = ({id='modal' , onClose= () => {}, children}) => {
-
+const Modal = ({ id = "modal", onClose = () => {}, children }) => {
   const handleOutSideClick = (e) => {
-    if(e.target.id == id) onClose();
-  }
+    if (e.target.id == id) onClose();
+  };
 
-  return(
-    //vai deixar a tela escura 
-  <div id="modal" className="modal" onClick={handleOutSideClick} >
-
-    <h1></h1>
-
-  </div>
+  return (
+    //vai deixar a tela escura
+    <div id="modal" className="modal" onClick={handleOutSideClick}>
+      <h1>Modal</h1>
+    </div>
   );
 };
 
